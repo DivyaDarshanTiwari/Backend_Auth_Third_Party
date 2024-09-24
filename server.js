80e961600cd0f92a2b94669e4b4b63620ca2d4d1
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-09-24 15:27:28 +0530

    weather api is added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 require("dotenv").config();
 const connectDB = require("./config/db"); // Assuming your connectDB is correctly set up.
 const user_data = require("./routes/user_data");
+const axios = require('axios');
 
 const app = express();
 const mongoose = require("mongoose");
@@ -31,6 +32,20 @@ app.get("/welcome", (req, res) => {
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/user", user_data);
+
+// Your WeatherAPI key
+const weatherApiKey = '35ecd18235524c63b7594841242409'; // Replace with your API key
+
+// Create a route to fetch weather data for Dehradun
+app.get('/weather', async (req, res) => {
+  try {
+    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=Dehradun`);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    res.status(500).json({ error: 'Error fetching weather data' });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
